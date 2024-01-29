a352605f3a18a4f1ecd187ba62fe171368fe7680
Das-maker <eliott.lemaire778@gmail.com>
2024-01-29 14:53:17 +0100

    terminal

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'view')));
 
 
@@ -12,6 +14,15 @@ app.get('/', (req, res) => {
 
 app.get('/terminal', (req, res) => {
   res.sendFile(path.join(__dirname, 'view', 'terminal.html'));
+});
+
+app.post('/execute-command', (req, res) => {
+  const { command } = req.body;
+  
+  // Here you can execute the command
+  // For demonstration, let's just echo back the command
+  console.log('Command received:', command);
+  res.json({ message: `Command received: ${command}` });
 });
 
 app.listen(port, () => {
