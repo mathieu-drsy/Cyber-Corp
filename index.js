c0dfb144c08ee8e0247051f0308e08b6e15c47a8
SNIR420 <lignieresa77@gmail.com>
2024-01-29 13:44:13 +0100

    Modif bdd v2

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const app = express();
 const port = 3000;
 
 // Création d'une base de données SQLite et d'une table simple
-const db = new sqlite3.Database('mydatabase.db');
+const db = new sqlite3.Database('save.db');
 db.serialize(() => {
   db.run("CREATE TABLE IF NOT EXISTS data (id INTEGER PRIMARY KEY, pseudo TEXT, score INTEGER, difficulté INTEGER, vie INTEGER, etage INTEGER, mdp TEXT)");
 });
 
 app.get('/', (req, res) => {
   // Exemple : insérer des données dans la base de données
-  db.run("INSERT INTO messages (content) VALUES (?)", ["Hello from SQLite!"]);
+  db.run("INSERT INTO data (pseudo, score, difficulté, vie, etage, mdp) VALUES (?, ?, ?, ?, ?, ?)", ["test", 420, 3, 3, 0, "test"]);
 
   // Récupérer des données depuis la base de données
-  db.all("SELECT * FROM messages", (err, rows) => {
+  db.all("SELECT * FROM data", (err, rows) => {
     if (err) {
       return console.error(err.message);
     }
