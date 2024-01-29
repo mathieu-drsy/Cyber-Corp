006200a972380a168d5e2422b99a299b1d112d1d
SNIR420 <lignieresa77@gmail.com>
2024-01-29 15:51:02 +0100

    maj

--- index.js
+++ index.js
@@ -1,61 +1,16 @@
-<<<<<<< HEAD
 const express = require('express');
 const initDb = require('./db_init');
 const { setupRoutes } = require('./routes');
 
-=======
-const express = require("express");
->>>>>>> main
 const app = express();
 const port = 3000;
-const path = require("path");
 
-<<<<<<< HEAD
 const db=initDb();
 
 setupRoutes(app, db);
 
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Fonctionnel sur http://localhost:${port}`);
 });
-=======
-app.use(express.json());
-
-app.use(express.static(path.join(__dirname, "view")));
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/terminal", (req, res) => {
-  res.sendFile(path.join(__dirname, "view", "terminal.html"));
-});
-
-app.post("/execute-command", (req, res) => {
-  const { command } = req.body;
-  let output = "";
-
-  // Execute specific commands
-  switch (command.trim()) {
-    case "help":
-      output =
-        "Available commands:\n- help: Display available commands\n- date: Show current date and time";
-      break;
-    case "date":
-      const currentDate = new Date().toLocaleString();
-      output = `Current date and time: ${currentDate}`;
-      break;
-    default:
-      output = `Command not found: ${command}`;
-      break;
-  }
-
-  // Log the command and send back the output
-  console.log("Command received:", command);
-  res.json({ message: output });
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
->>>>>>> main
