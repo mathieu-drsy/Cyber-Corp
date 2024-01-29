de57704f440c06ed70d937fc7250e8cedaa76ad9
SNIR420 <lignieresa77@gmail.com>
2024-01-29 15:07:07 +0100

    test modif

--- index.js
+++ index.js
@@ -36,39 +36,24 @@ app.get('/q_raw', (req, res) => {
   });
 });
 
-app.post('/difficulteNovice', (req, res) => {
-  // Exemple : insérer des données dans la base de données avec difficulté 1
-  db.run("INSERT INTO data (pseudo, score, difficulté, vie, etage, mdp) VALUES (?, ?, ?, ?, ?, ?)", ["Novice", 420, 1, 3, 0, "test"], (err) => {
-    if (err) {
-      return res.status(500).send(err.message);
-    }
+app.post('/difficulte', (req, res) => {
+  const { difficulty } = req.body;
 
-    res.status(200).send("Difficulté Novice sélectionnée");
-  });
-});
-
-// Ajoutez cette route après la création de la base de données dans votre fichier index.js
-app.post('/difficulteIntermediaire', (req, res) => {
-  // Exemple : insérer des données dans la base de données avec difficulté 1
-  db.run("INSERT INTO data (pseudo, score, difficulté, vie, etage, mdp) VALUES (?, ?, ?, ?, ?, ?)", ["Intermediaire", 420, 2, 3, 0, "test"], (err) => {
-    if (err) {
-      return res.status(500).send(err.message);
-    }
-
-    res.status(200).send("Difficulté Intermediaire sélectionnée");
-  });
-});
-
-// Ajoutez cette route après la création de la base de données dans votre fichier index.js
-app.post('/difficulteExpert', (req, res) => {
-  // Exemple : insérer des données dans la base de données avec difficulté 1
-  db.run("INSERT INTO data (pseudo, score, difficulté, vie, etage, mdp) VALUES (?, ?, ?, ?, ?, ?)", ["Expert", 420, 3, 3, 0, "test"], (err) => {
-    if (err) {
-      return res.status(500).send(err.message);
-    }
-
-    res.status(200).send("Difficulté Expert sélectionnée");
-  });
+  if (difficulty && typeof difficulty === 'number') {
+      // Utilisez la difficulté reçue pour insérer des données dans la base de données
+      const pseudo = getDifficultyLabel(difficulty);  // Remplacez cela par la logique appropriée pour obtenir le pseudo
+      db.run("INSERT INTO data (pseudo, score, difficulté, vie, etage, mdp) VALUES (?, ?, ?, ?, ?, ?)",
+          [pseudo, 420, difficulty, 3, 0, "test"],
+          (err) => {
+              if (err) {
+                  return res.status(500).send(err.message);
+              }
+              res.status(200).send(`Difficulté ${pseudo} sélectionnée`);
+          }
+      );
+  } else {
+      res.status(400).send('Erreur de difficulté');
+  }
 });
 
 app.get('/view', (req, res) => {
