4f9ec1c729685aced1077325bc91a8128a9a1af1
Adam <ferkousadam@yahoo.fr>
2024-01-30 11:23:18 +0100

    connexion

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/terminal", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.join(__dirname, "view", "login.html"));
+  res.sendFile(path.join(__dirname, "login", "login.html"));
 });
 
 app.post("/execute-command", (req, res) => {
