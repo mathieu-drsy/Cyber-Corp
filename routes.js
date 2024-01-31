ee2fb8daedd66199939f44bff0842cff709563af
Das-maker <eliott.lemaire778@gmail.com>
2024-01-31 10:54:50 +0100

    c

--- routes.js
+++ routes.js
@@ -1,27 +1,12 @@
-const path = require('path');
-const bcrypt = require('bcrypt');
+const path = require("path");
+const bcrypt = require("bcrypt");
 
 function setupRoutes(app, db) {
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'view', 'index.html'));
-    // Exemple : insérer des données dans la base de données
-    //db.run("INSERT INTO data (pseudo, score, difficulté, vie, etage, mdp) VALUES (?, ?, ?, ?, ?, ?)", ["test", 420, 3, 3, 0, "test"]);
-
-
-    // Récupérer des données depuis la base de données
-    /*db.all("SELECT * FROM data", (err, rows) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      // Envoyer les données en réponse HTTP
-      res.send(rows);
-    });*/
-
-    //SELECTION QUESTIONS
-    //SELECT {3 questions} FROM questions WHERE questions.difficulte EQUALS data.difficulte
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "view", "index.html"));
   });
 
-  app.get('/q_raw', (req, res) => {
+  app.get("/q_raw", (req, res) => {
     // Récupérer des données depuis la base de données
     db.all("SELECT * FROM questions", (err, rows) => {
       if (err) {
@@ -32,34 +17,24 @@ function setupRoutes(app, db) {
     });
   });
 
-  app.post('/difficulte', (req, res) => {
+  app.post("/difficulte", (req, res) => {
     const difficultyValue = req.body.difficulty;
     // Utilisez la valeur de difficulté comme vous le souhaitez
     // Exemple : insérer des données dans la base de données
-    db.run("INSERT INTO data (pseudo, score, difficulté, vie, etage, mdp) VALUES (?, ?, ?, ?, ?, ?)", ["MODIF", 420, difficultyValue, 3, 0, "test"], (err) => {
-      if (err) {
-        return res.status(500).send(err.message);
-      }
+    db.run(
+      "INSERT INTO data (pseudo, score, difficulté, vie, etage, mdp) VALUES (?, ?, ?, ?, ?, ?)",
+      ["MODIF", 420, difficultyValue, 3, 0, "test"],
+      (err) => {
+        if (err) {
+          return res.status(500).send(err.message);
+        }
 
-      res.status(200).send(`Difficulté ${difficultyValue} sélectionnée`);
-    });
+        res.status(200).send(`Difficulté ${difficultyValue} sélectionnée`);
+      }
+    );
   });
 
-  app.post('/setScore', (req, res) => {
-    const scoreValue = req.body.score;
-    // Utilisez la valeur de difficulté comme vous le souhaitez
-    // Exemple : insérer des données dans la base de données
-    /*db.run("INSERT INTO data (pseudo, score, difficulté, vie, etage, mdp) VALUES (?, ?, ?, ?, ?, ?)", ["MODIF", 420, difficultyValue, 3, 0, "test"], (err) => {
-      if (err) {
-        return res.status(500).send(err.message);
-      }
-
-      res.status(200).send(`Difficulté ${difficultyValue} sélectionnée`);
-    });*/
-    console.log(scoreValue);
-  });
-
-  app.post('/setUser', async (req, res) => {
+  app.post("/connexion", async (req, res) => {
     const usernameValue = req.body.username;
     const passwordValue = req.body.password;
 
@@ -70,30 +45,52 @@ function setupRoutes(app, db) {
       if (existingUser) {
         // 2. L'utilisateur existe
         // 3. Vérifier le mot de passe
-        const passwordMatch = await bcrypt.compare(passwordValue, existingUser.mdp);
+        const passwordMatch = await bcrypt.compare(
+          passwordValue,
+          existingUser.mdp
+        );
 
         if (passwordMatch) {
           // Le mot de passe correspond, afficher un log et envoyer une réponse appropriée
-          console.log('Connexion réussie');
+          console.log("Connexion réussie");
           //res.status(200).send('Connexion réussie');
         } else {
           // Le mot de passe ne correspond pas, afficher un log et envoyer une réponse appropriée
-          console.log('Mot de passe incorrect');
-          //res.status(401).send('Mot de passe incorrect');
+          console.log("Mot de passe incorrect");
+          res.sendFile(path.join(__dirname, "login", "login.html"));
         }
+      } else {
+        console.log("L'utilisateur n'existe pas");
+        res.sendFile(path.join(__dirname, "login", "login.html"));
+      }
+    } catch (error) {}
+  });
+
+  app.post("/inscription", async (req, res) => {
+    const usernameValue = req.body.username;
+    const passwordValue = req.body.password;
+    //console.log(passwordValue);
+    try {
+      // 1. Récupérer l'utilisateur existant par le nom d'utilisateur
+      const existingUser = await getUserByUsername(usernameValue);
+      if (existingUser) {
+        console.log("Utilisateur existe déjà");
       } else {
         // L'utilisateur n'existe pas, afficher un log et envoyer une réponse appropriée
         const hashedPassword = await bcrypt.hash(passwordValue, 10);
-        db.run("INSERT INTO data (pseudo, score, difficulté, vie, etage, mdp) VALUES (?, ?, ?, ?, ?, ?)",
-          [usernameValue, 0, 0, 0, 0, hashedPassword], (err) => {
+        db.run(
+          "INSERT INTO data (pseudo, score, difficulte, vie, etage, mdp) VALUES (?, ?, ?, ?, ?, ?)",
+          [usernameValue, 0, 0, 0, 0, hashedPassword],
+          (err) => {
             if (err) {
               return res.status(500).send(err.message);
             }
 
-            console.log('Utilisateur ajouté avec succès');
-            res.status(200).send('Utilisateur ajouté avec succès');
-          });
-        console.log(`Utilisateur ${usernameValue} ajouté`);
+            console.log("Utilisateur ajouté avec succès");
+            res.status(200).send("Utilisateur ajouté avec succès");
+          }
+        );
+
         //res.status(404).send('Utilisateur non trouvé');
       }
     } catch (error) {
@@ -115,14 +112,179 @@ function setupRoutes(app, db) {
     });
   }
 
-  app.get('/transition', (req, res) => {
-    // Utilisez la méthode sendFile pour renvoyer la page index.html située dans le répertoire 'view'
-    res.sendFile(path.join(__dirname, 'view/transition', 'transition.html'));
+  app.post("/setVie", (req, res) => {
+    const { pseudo, remainingLives } = req.body;
+
+    // Mise à jour de la valeur de la colonne 'vie' dans la base de données
+    const sql = "UPDATE data SET vie = ? WHERE pseudo = ?";
+
+    db.run(sql, [remainingLives, pseudo], (err) => {
+      if (err) {
+        console.error("Error updating lives in the database:", err);
+        return res
+          .status(500)
+          .json({
+            success: false,
+            error: "Error updating lives in the database",
+          });
+      }
+
+      console.log(
+        `Updated lives for ${pseudo} - Remaining Lives: ${remainingLives}`
+      );
+      res.json({ success: true });
+    });
   });
 
-  app.get('/terminal', (req, res) => {
+  app.get("/getVie/:pseudo", (req, res) => {
+    const pseudo = req.params.pseudo;
+
+    // Sélection du nombre de vies depuis la base de données
+    const sql = "SELECT vie FROM data WHERE pseudo = ?";
+
+    db.get(sql, [pseudo], (err, row) => {
+      if (err) {
+        console.error("Error getting lives from the database:", err);
+        return res
+          .status(500)
+          .json({
+            success: false,
+            error: "Error getting lives from the database",
+          });
+      }
+
+      if (row) {
+        console.log(
+          `Retrieved lives for ${pseudo} - Remaining Lives: ${row.vie}`
+        );
+        res.json({ success: true, lives: row.vie });
+      } else {
+        console.log(`User ${pseudo} not found in the database`);
+        res
+          .status(404)
+          .json({ success: false, error: "User not found in the database" });
+      }
+    });
+  });
+
+  app.get("/getScore/:pseudo", (req, res) => {
+    const pseudo = req.params.pseudo;
+    // Sélection du nombre de vies depuis la base de données
+    const sql = "SELECT score FROM data WHERE pseudo = ?";
+
+    db.get(sql, [pseudo], (err, row) => {
+      if (err) {
+        console.error(
+          "Erreur lors de la mise à jour du score dans la db:",
+          err
+        );
+        return res
+          .status(500)
+          .json({
+            success: false,
+            error: "Erreur lors de la lecture du score dans la db",
+          });
+      }
+
+      if (row) {
+        //console.log(`Score de ${pseudo} - Score: ${row.score}`);
+        res.json({ score: row.score });
+      } else {
+        console.log(`L'utilisateur ${pseudo} n'est pas dans la db`);
+        res
+          .status(404)
+          .json({
+            success: false,
+            error: "Utilisateur introuvable dans la db",
+          });
+      }
+    });
+  });
+
+  app.post("/setScore", (req, res) => {
+    const { pseudo, userScore } = req.body;
+    console.log(userScore);
+    // Mise à jour de la valeur de la colonne 'vie' dans la base de données
+    const sql = "UPDATE data SET score = ? WHERE pseudo = ?";
+
+    db.run(sql, [userScore, pseudo], (err) => {
+      if (err) {
+        console.error("Erreur lors de la mise à jour du score:", err);
+        return res
+          .status(500)
+          .json({ success: false, error: "Erreur de la mise à jour de la db" });
+      }
+
+      console.log(`Mise à jour du Score de ${pseudo} - Score: ${userScore}`);
+      res.json({ success: true });
+    });
+  });
+
+  app.get("/getEtage/:pseudo", (req, res) => {
+    const pseudo = req.params.pseudo;
+    // Sélection du nombre de vies depuis la base de données
+    const sql = "SELECT etage FROM data WHERE pseudo = ?";
+
+    db.get(sql, [pseudo], (err, row) => {
+      if (err) {
+        console.error(
+          "Erreur lors de la mise à jour du score dans la db:",
+          err
+        );
+        return res
+          .status(500)
+          .json({
+            success: false,
+            error: "Erreur lors de la lecture du score dans la db",
+          });
+      }
+
+      if (row) {
+        res.json({ etage: row.etage });
+      } else {
+        console.log(`L'utilisateur ${pseudo} n'est pas dans la db`);
+        res
+          .status(404)
+          .json({
+            success: false,
+            error: "Utilisateur introuvable dans la db",
+          });
+      }
+    });
+  });
+
+  app.post("/setEtage", (req, res) => {
+    const { pseudo, userEtage } = req.body;
+    console.log(userEtage);
+    // Mise à jour de la valeur de la colonne 'vie' dans la base de données
+    const sql = "UPDATE data SET etage = ? WHERE pseudo = ?";
+
+    db.run(sql, [userEtage, pseudo], (err) => {
+      if (err) {
+        console.error("Erreur lors de la mise à jour du score:", err);
+        return res
+          .status(500)
+          .json({ success: false, error: "Erreur de la mise à jour de la db" });
+      }
+
+      console.log(`Mise à jour du Score de ${pseudo} - Score: ${userEtage}`);
+      res.json({ success: true });
+    });
+  });
+
+  app.get("/transition", (req, res) => {
     // Utilisez la méthode sendFile pour renvoyer la page index.html située dans le répertoire 'view'
-    res.sendFile(path.join(__dirname, 'view', 'terminal.html'));
+    res.sendFile(path.join(__dirname, "view/transition", "transition.html"));
+  });
+
+  app.get("/terminal", (req, res) => {
+    // Utilisez la méthode sendFile pour renvoyer la page index.html située dans le répertoire 'view'
+    res.sendFile(path.join(__dirname, "view", "terminal.html"));
+  });
+
+  app.get("/ter", (req, res) => {
+    // Utilisez la méthode sendFile pour renvoyer la page index.html située dans le répertoire 'view'
+    res.sendFile(path.join(__dirname, "view", "terminal_test.html"));
   });
 
   app.get("/login", (req, res) => {
@@ -141,26 +303,30 @@ function setupRoutes(app, db) {
     res.sendFile(path.join(__dirname, "view", "profil.html"));
   });
 
-  app.get('/get-questions', (req, res) => {
+  app.get("/get-questions", (req, res) => {
     const difficulty = req.query.difficulty; // Récupérer la difficulté à partir de la query string
-    const sql = "SELECT * FROM questions WHERE difficulte = ? ORDER BY RANDOM() LIMIT 3"; // Sélectionner 3 questions aléatoires
+    const sql =
+      "SELECT * FROM questions WHERE difficulte = ? ORDER BY RANDOM() LIMIT 3"; // Sélectionner 3 questions aléatoires
 
     db.all(sql, [parseInt(difficulty)], (err, rows) => {
       if (err) {
-        return res.status(500).send('Une erreur est survenue lors de la récupération des questions.');
+        return res
+          .status(500)
+          .send(
+            "Une erreur est survenue lors de la récupération des questions."
+          );
       }
       res.send(rows); // Renvoyer les données récupérées
     });
   });
 
-
-  app.get('/get3q', (req, res) => {
+  app.get("/get3q", (req, res) => {
     // Utilisez la méthode sendFile pour renvoyer la page index.html située dans le répertoire 'view'
-    res.sendFile(path.join(__dirname, 'view', 'get-q.html'));
+    res.sendFile(path.join(__dirname, "view", "get-q.html"));
   });
 
   // Route pour arrêter le serveur
-  app.post('/clear-questions', (req, res) => {
+  app.post("/clear-questions", (req, res) => {
     // Nettoyer la table 'questions'
     db.run("DELETE FROM questions");
     console.log('Table "questions" vidée.');
@@ -204,7 +370,6 @@ function setupRoutes(app, db) {
     console.log("Command received:", command);
     res.json({ message: output, correct });
   });
-
 }
 
 module.exports = { setupRoutes };
